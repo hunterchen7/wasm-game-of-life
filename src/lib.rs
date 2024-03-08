1db3d8d7d0aa2a296489a4325b29343b62d8a5e5
hunterchen7 <hello@hunterchen.ca>
2024-03-08 02:38:50 -0500

    adding js stuff mostly

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,146 @@
+extern crate cfg_if;
+extern crate wasm_bindgen;
+extern crate web_sys;
+
 mod utils;
+
+use std::fmt;
+use wasm_bindgen::prelude::*;
+use web_sys::console;
 
 use wasm_bindgen::prelude::*;
 
-#[wasm_bindgen]
-extern "C" {
-    fn alert(s: &str);
+// based off of https://rustwasm.github.io/docs/book/game-of-life/implementing.html
+
+impl fmt::Display for Universe {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        for line in self.cells.as_slice().chunks(self.width as usize) {
+            for &cell in line {
+                let symbol = if cell == Cell::Dead { '◻' } else { '◼' };
+                write!(f, "{}", symbol)?;
+            }
+            writeln!(f)?;
+        }
+        Ok(())
+    }
 }
 
 #[wasm_bindgen]
-pub fn greet() {
-    alert("Hello, wasm-game-of-life!");
+#[repr(u8)]
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+pub enum Cell {
+    Dead = 0,
+    Alive = 1,
+}
+
+#[wasm_bindgen]
+pub struct Universe {
+    width: u32,
+    height: u32,
+    cells: Vec<Cell>, // [i * width + j] to index at [i][j]
+}
+
+const NEIGHBORS: [(i32, i32); 8] = [
+    (-1, -1),
+    (-1, 0),
+    (-1, 1),
+    (0, -1),
+    (0, 1),
+    (1, -1),
+    (1, 0),
+    (1, 1),
+];
+
+#[wasm_bindgen]
+impl Universe {
+    fn get_index(&self, row: u32, column: u32) -> usize {
+        (row * self.width + column) as usize
+    }
+
+    fn live_neighbor_count(&self, row: u32, column: u32) -> u8 {
+        let mut count = 0;
+        for (dx, dy) in NEIGHBORS {
+            // continue if out of bounds
+            // in the implemented game, the universe wraps around, but I want it to expand!!!!!
+            // TO INFINITY AND BEYOND!!!! 🚀🚀🚀🚀 (or at least until the memory runs out)
+            if (dx < 0 && row == 0) || (dy < 0 && column == 0)
+                || (dx > 0 && row >= self.height - 1) || (dy > 0 && column >= self.width - 1) {
+                continue;
+            }
+            let x = (row as i32 + dx) as u32;
+            let y = (column as i32 + dy) as u32;
+            if x < self.height && y < self.width {
+                let idx = self.get_index(x, y);
+                count += self.cells[idx] as u8;
+            }
+        }
+        count
+    }
+
+    pub fn new(width: u32, height: u32) -> Universe {
+        let cells = (0..width * height)
+            .map(|i| {
+                if i % 2 == 0 || i % 7 == 0 {
+                    Cell::Alive
+                } else {
+                    Cell::Dead
+                }
+            })
+            .collect();
+
+        Universe {
+            width,
+            height,
+            cells,
+        }
+    }
+
+    pub fn tick(&mut self) {
+        let mut next = self.cells.clone();
+
+        for row in 0..self.height {
+            for col in 0..self.width {
+                let idx = self.get_index(row, col);
+                let cell = self.cells[idx];
+                let live_neighbors = self.live_neighbor_count(row, col);
+
+                let next_cell = match (cell, live_neighbors) {
+                    // Rule 1: Any live cell with < 2 live neighbours dies by underpopulation
+                    (Cell::Alive, x) if x < 2 => Cell::Dead,
+                    // Rule 2: Any live cell with 2 or 3 live neighbours lives on
+                    (Cell::Alive, 2) | (Cell::Alive, 3) => Cell::Alive,
+                    // Rule 3: Any live cell with > 3 live neighbours dies by overpopulation
+                    (Cell::Alive, x) if x > 3 => Cell::Dead,
+                    // Rule 4: Any dead cell with 3 live neighbours becomes alive by reproduction
+                    (Cell::Dead, 3) => Cell::Alive,
+                    // All other cells remain in the same state.
+                    (otherwise, _) => otherwise,
+                };
+                next[idx] = next_cell;
+            }
+        }
+
+        self.cells = next;
+    }
+
+    pub fn width(&self) -> u32 {
+        self.width
+    }
+
+    pub fn height(&self) -> u32 {
+        self.height
+    }
+
+    pub fn cells(&self) -> *const Cell {
+        self.cells.as_ptr()
+    }
+
+    pub fn render(&self) -> String {
+        self.to_string()
+    }
+}
+
+#[wasm_bindgen]
+pub fn wasm_memory() -> JsValue {
+    wasm_bindgen::memory()
 }
